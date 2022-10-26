14d81b3c3b5cb584e32c004fab541227bb991c03
Vadim_B <bachkovvadim@gmail.com>
2022-10-26 14:06:43 +0300

    Added modifications

--- gitHubUserFinder/index.js
+++ gitHubUserFinder/index.js
@@ -4,26 +4,24 @@ const userSection = document.querySelector(".showUser");
 function changeInfo(data) {
     userSection.style.display = "block";
   
-    let img = document.querySelector(".userPhoto");
     let userInfo = document.querySelector("#userInfo");
   
-    userInfo.innerHTML = `<li><span>Name:</span> ${data.name}</li>
-    <li><span>Bio:</span> ${data.bio}</li>
-    <li><span>Location:</span> ${data.location}</li>
-    <li><span>Company:</span> ${data.company}</li>`;
+    userInfo.innerHTML = `<li><span>Name:</span> ${data.name}</li>`
+    // <li><span>Bio:</span> ${data.bio}</li>
+    // <li><span>Location:</span> ${data.location}</li>
+    // <li><span>Company:</span> ${data.company}</li>`;
   
-    img.innerHTML = `<img src="${data.avatar_url}">`;
+    // img.innerHTML = `<img src="${data.avatar_url}">`;
   }
 
 class GitHub {
-    constructor() {
-      this.client_id = "f87f791e3012f3924b6b";
-      this.client_secret = "15656eee74dc31b6fcd74d4961c31fb842e5e01e";
-    }
+    // constructor() {
+
+    // }
   
     async getUser(user) {
       const profileResponse = await fetch(
-        `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+        `https://api.github.com/users/${user}`
       );
       const profileData = await profileResponse.json();
       return {
