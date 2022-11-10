009ed32c68d9c3b7de6514e489ce39927b1539d8
Vadim_B <bachkovvadim@gmail.com>
2022-11-10 16:08:07 +0300

    Added new modifications

--- movieApp/index.js
+++ movieApp/index.js
@@ -1,42 +1,52 @@
 
+const API_KEY = `3ef4ab9e`;
 
-const MOVIE_API_KEY = `ae8115887a332015cc3739c9ecb9b70d`;
+const API_URL = `https://www.omdbapi.com/`;
 
-const API_URL = `https://api.themoviedb.org/3/movie/popular?`;
+let page = 1;
 
-const IMAGE_URL = `https://image.tmdb.org/t/p/w1080`;
 
-// select dom Elements
 let mostPopularDocument = document.querySelector(".most-popular");
-let movieContainer =document.querySelector(".movie-container");
+let movieContainer = document.querySelector(".movie-container");
 let modalContainer = document.querySelector(".modal-container");
 let close = document.querySelector(".close");
 let movieTitle = document.querySelector(".movie-title");
 let movieDetail = document.querySelector(".movie-detail");
+const movies = document.querySelector('#movies');
 
 
+
+
+const inputField = document.querySelector(".entered-field");
+
+inputField.addEventListener('click', () => {
+    let movieName = inputField.value;
+    if (movieName.trim() != 0) {
+      return biuldMoviesContainer(results);
+    } else return;
+    
+})
 
 
 //when you click on the image box
 movieContainer.addEventListener("click", (event) => {
 
     if (event.target.classList[0] == "movie-image") {
-        //gets the title of the clicked image
+
         let title = event.target.parentElement.children[2].children[0].innerText
-        //gets the clicked image overview which is the hidden input
+
         let overview = event.target.parentElement.children[0].value;
 
-        // set the title and and the deatils
         movieTitle.innerHTML = title;
         movieDetail.innerHTML = overview;
 
-        // show the modal
+
         modalContainer.classList = "modal-container show";
     }
 
 })
 
-// click to overly to close the modal
+
 modalContainer.addEventListener("click", (event) => {
 
     if (event.target.classList[0] == "modal-container") {
@@ -49,45 +59,39 @@ close.addEventListener("click", () => {
     modalContainer.classList = "modal-container hide";
 })
 
-// funtion that shows all movies to the DOM
-const biuldMoviesContainer = (movies) => {
 
-    mostPopularDocument.innerHTML = "";
+function biuldMoviesContainer(result) {
 
-    movies.forEach(movie => {
+    movies.style.display = "block";
 
-        mostPopularDocument.innerHTML += `<div class="movie">
-        <input type="hidden" value="${movie.overview}">
-        <img class="movie-image" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="">
+    mostPopularDocument.innerHTML += `<div class="movie">
+        
+        <img class="movie-image" src="https://image.tmdb.org/t/p/w500$%7Bmovie${result.Poster}">
        <div class="info">
-        <span class="movie-title">${movie.original_title}</span>
+        <span class="movie-title">${result.Title}</span>
         
            <div class="counts">
             <div class="vote-average">
-                <span>${movie.vote_average}</span>
+                <span>${result.ratings}</span>
             </div>
             <div class="release-date">
-                <span>${movie.release_date}</span>
+                <span>${result.released}</span>
             </div>
            
            </div>
         </div>
     </div>`
 
-    })
-
 }
 
 
+async function showMovie(movieName,page) {
 
-const getMostPopularMoviews = async () => {
-
-    const request = await fetch(`${API_URL}api_key=${MOVIE_API_KEY}&page=1`);
-    
-
-    const { results } = await request.json();
+    const request = await fetch(`${API_URL}?apiKey=${API_KEY}&s=${movieName}&page=${page}`);
+   
+    const results = await request.json();
 
     biuldMoviesContainer(results);
 }
 
-getMostPopularMoviews();
+showMovie();
