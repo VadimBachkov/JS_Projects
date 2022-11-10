022b5f8028c4b4da3dfdeb56e89db62d32a3fbd1
Vadim_Bachkov <79111473+VadimBachkov@users.noreply.github.com>
2022-11-10 16:39:20 +0300

    Update script.js

--- Movie Finder/script.js
+++ Movie Finder/script.js
@@ -12,7 +12,7 @@ async function loadMovies(serchMovie){
     const res = await fetch(`${URL}`);
     const data = await res.json();
     // console.log(data.Search);
-    if(data.Response == "true") displayMovieList(data.Search);
+    if(data.Response == "True") displayMovieList(data.Search);
 }
 
 function findMovies(){
